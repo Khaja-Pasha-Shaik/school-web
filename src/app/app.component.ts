830e7895c8722efbd70dc13583d3b4b0cb3311f2
shaikkhajapasha94@gmail.com <Khaja Pasha Shaik>
2024-11-28 18:29:19 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit, QueryList, ViewChildren } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'school-web';
+export class AppComponent implements OnInit {
+  @ViewChildren('mediaPlayLists') public mediaPlayLists!: QueryList<ElementRef>;
+
+  title = 'school';
+  
+  constructor() {
+
+  }
+
+  ngOnInit(): void {
+    
+  }
+  swipeRow(clicked: string, row: number) {
+    if (clicked == 'Left') {
+        (this.mediaPlayLists.get(row) as any).nativeElement.scrollLeft -= 340;
+    }
+    else {
+        (this.mediaPlayLists.get(row) as any).nativeElement.scrollLeft += 340;
+    }
+}
 }
